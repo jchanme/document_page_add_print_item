9983e678e6175f4afca9307f14ee24f1245c8970
jchanme <39323414+jchanme@users.noreply.github.com>
2020-02-24 19:54:06 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Document Page - Letters",
+    "version": "11.0.1.0.0",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://www.safyric.com",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Others",
+    "depends": [
+        'mgmtsystem_manual'
+    ],
+    "data": [
+        'views/document_page_letters.xml',
+    ],
+    'installable': True,
+}
